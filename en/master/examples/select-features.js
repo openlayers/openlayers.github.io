166ffca9304d1826b1a0e5eee20e6294531b1924
Tom Payne <twpayne@gmail.com>
2014-03-23 03:07:23 +0100

    Updates

--- en/master/examples/select-features.js
+++ en/master/examples/select-features.js
@@ -6,23 +6,37 @@ var vector = new ol.layer.Vector({
   source: new ol.source.GeoJSON({
     projection: 'EPSG:3857',
     url: 'data/geojson/countries.geojson'
-  }),
-  style: new ol.style.Style({
-    fill: new ol.style.Fill({
-      color: 'rgba(255,255,255,0.25)'
-    }),
-    stroke: new ol.style.Stroke({
-      color: '#6666ff'
-    })
   })
 });
 
+var styleCache = {};
 var select = new ol.interaction.Select({
-  style: new ol.style.Style({
-    fill: new ol.style.Fill({
-      color: 'rgba(255,255,255,0.5)'
-    })
-  })
+  style: function(feature, resolution) {
+    var text = feature.get('name');
+    if (!styleCache[text]) {
+      styleCache[text] = [new ol.style.Style({
+        fill: new ol.style.Fill({
+          color: 'rgba(255,0,0,0.3)'
+        }),
+        stroke: new ol.style.Stroke({
+          color: 'rgba(255,0,0,1)',
+          size: 2
+        }),
+        text: new ol.style.Text({
+          font: '12px Calibri,sans-serif',
+          text: text,
+          fill: new ol.style.Fill({
+            color: '#000'
+          }),
+          stroke: new ol.style.Stroke({
+            color: '#fff',
+            width: 3
+          })
+        })
+      })];
+    }
+    return styleCache[text];
+  }
 });
 
 var map = new ol.Map({
