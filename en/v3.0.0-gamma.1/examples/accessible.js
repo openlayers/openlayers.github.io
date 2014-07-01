8bd6ecafd63508ef391cbc0c89fc158d3dc30d80
Andreas Hocevar <andreas.hocevar@gmail.com>
2014-07-01 10:26:22 +0200

    Updates

---
+++ en/v3.0.0-gamma.1/examples/accessible.js
@@ -0,0 +1,20 @@
+var map = new ol.Map({
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  renderer: exampleNS.getRendererFromQueryString(),
+  target: 'map',
+  view: new ol.View({
+    center: [0, 0],
+    zoom: 2
+  })
+});
+
+jQuery('#map').after('<button type="button" ' +
+    'onclick="map.getView().setZoom(map.getView().getZoom() - 1);">' +
+    'Zoom out</button>');
+jQuery('#map').after('<button type="button" ' +
+    'onclick="map.getView().setZoom(map.getView().getZoom() + 1);">' +
+    'Zoom in</button>');
