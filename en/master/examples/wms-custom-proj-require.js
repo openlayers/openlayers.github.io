14ba949e2e396997f207e67a2721ace021d76d6e
Andreas Hocevar <andreas.hocevar@gmail.com>
2014-07-10 13:36:55 +0200

    Updates

--- en/master/examples/wms-custom-proj-require.js
+++ en/master/examples/wms-custom-proj-require.js
@@ -5,4 +5,5 @@ goog.require('ol.control');
 goog.require('ol.control.ScaleLine');
 goog.require('ol.layer.Tile');
 goog.require('ol.proj');
+goog.require('ol.proj.Projection');
 goog.require('ol.source.TileWMS');
