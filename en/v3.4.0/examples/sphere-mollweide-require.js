ba76c14fb40f70464e15a08bf176424d8990508d
Bart van den Eijnden <bartvde@boundlessgeo.com>
2015-03-27 11:45:19 +0100

    Updates

---
+++ en/v3.4.0/examples/sphere-mollweide-require.js
@@ -0,0 +1,6 @@
+goog.require('ol.Graticule');
+goog.require('ol.Map');
+goog.require('ol.View');
+goog.require('ol.layer.Vector');
+goog.require('ol.proj.Projection');
+goog.require('ol.source.GeoJSON');
