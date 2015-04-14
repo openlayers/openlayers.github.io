13b15aebb8da421500900ea72df864c61eed7891
Frederic Junod <frederic.junod@camptocamp.com>
2015-04-14 17:04:45 +0200

    Updates

--- en/master/build/ol-debug.js
+++ en/master/build/ol-debug.js
@@ -1,6 +1,6 @@
 // OpenLayers 3. See http://openlayers.org/
 // License: https://raw.githubusercontent.com/openlayers/ol3/master/LICENSE.md
-// Version: v3.4.0-259-gee67a9d
+// Version: v3.4.0-283-g8b0179b
 
 (function (root, factory) {
   if (typeof define === "function" && define.amd) {
