fd7aabd19c4aa299b09efd2518332a9b2f9ac905
Andreas Hocevar <andreas.hocevar@gmail.com>
2016-02-24 10:01:26 +0100

    Updates

---
+++ en/v3.14.0/examples/render-geometry-require.js
@@ -0,0 +1,7 @@
+goog.require('ol.geom.LineString');
+goog.require('ol.geom.Point');
+goog.require('ol.geom.Polygon');
+goog.require('ol.render');
+goog.require('ol.style.Circle');
+goog.require('ol.style.Fill');
+goog.require('ol.style.Stroke');
