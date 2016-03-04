586123c8b54dfb4a2c402a1754952af1bf7404dd
Andreas Hocevar <andreas.hocevar@gmail.com>
2016-03-04 09:42:56 +0100

    Updates

---
+++ en/v3.14.2/examples/wms-capabilities.js
@@ -0,0 +1,8 @@
+var parser = new ol.format.WMSCapabilities();
+
+fetch('data/ogcsample.xml').then(function(response) {
+  return response.text();
+}).then(function(text) {
+  var result = parser.read(text);
+  document.getElementById('log').innerText = JSON.stringify(result, null, 2);
+});
