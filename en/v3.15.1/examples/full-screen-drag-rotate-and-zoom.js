3f849757b82579f458db5fe9edf221994ff0b272
Frederic Junod <frederic.junod@camptocamp.com>
2016-04-07 17:02:03 +0200

    Updates

---
+++ en/v3.15.1/examples/full-screen-drag-rotate-and-zoom.js
@@ -0,0 +1,23 @@
+var map = new ol.Map({
+  controls: ol.control.defaults().extend([
+    new ol.control.FullScreen()
+  ]),
+  interactions: ol.interaction.defaults().extend([
+    new ol.interaction.DragRotateAndZoom()
+  ]),
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.BingMaps({
+        key: 'AkGbxXx6tDWf1swIhPJyoAVp06H0s0gDTYslNWWHZ6RoPqMpB9ld5FY1WutX8UoF',
+        imagerySet: 'Aerial'
+      })
+    })
+  ],
+  // Use the canvas renderer because it's currently the fastest
+  target: 'map',
+  view: new ol.View({
+    center: [-33519607, 5616436],
+    rotation: -Math.PI / 8,
+    zoom: 8
+  })
+});
