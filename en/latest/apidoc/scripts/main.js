1b402edc758e61356f0363d3517ba7d9fdc0b62d
Andreas Hocevar <andreas.hocevar@gmail.com>
2016-05-18 09:09:33 +0200

    Re-apply latest version warning code

--- en/latest/apidoc/scripts/main.js
+++ en/latest/apidoc/scripts/main.js
@@ -53,20 +53,64 @@ $(function () {
     $(window).on('resize', _onResize);
     _onResize();
 
-    // create source code links to github
-    var srcLinks = $('div.tag-source');
-    var masterSearch = window.location.href.match(/\/([^\/]*\/)apidoc\//);
-    if (masterSearch && masterSearch.length) {
-      var branch = masterSearch[1];
-      srcLinks.each(function(i, el) {
-        var textParts = el.innerHTML.trim().split(', ');
-        var link = 'https://github.com/openlayers/ol3/blob/' + branch +
-            textParts[0];
-        el.innerHTML = '<a href="' + link + '">' + textParts[0] + '</a>, ' +
-            '<a href="' + link + textParts[1].replace('line ', '#l') + '">' +
-            textParts[1] + '</a>';
-      });
-    }
+    // warn about outdated version
+    var packageUrl = 'https://raw.githubusercontent.com/openlayers/openlayers.github.io/build/package.json';
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', packageUrl);
+    xhr.onload = function(response) {
+      var json = JSON.parse(xhr.responseText);
+      var latestVersion = json.version;
+      var url = window.location.href;
+      var branchSearch = url.match(/\/v([^\/]*)\/apidoc\//);
+      var currentVersion = branchSearch ? branchSearch[1] : latestVersion;
+      var cookieText = 'dismissed=-' + latestVersion + '-';
+      var dismissed = document.cookie.indexOf(cookieText) != -1;
+      if (!dismissed && /^[0-9\.]*$/.test(currentVersion) && currentVersion != latestVersion) {
+        var link = url.replace(branchSearch[0], '/latest/apidoc/');
+        var linkXhr = new XMLHttpRequest();
+        linkXhr.open('HEAD', link);
+        linkXhr.onload = function(response) {
+          var a = document.getElementById('latest-link');
+          a.href = linkXhr.status == 200 ? link : '../../latest/apidoc/';
+        };
+        linkXhr.send();
+        var latestCheck = document.createElement('div');
+        latestCheck.style.marginTop = '-10px';
+        latestCheck.style.marginBottom = '10px';
+        latestCheck.className = 'alert alert-warning alert-dismissable';
+        latestCheck.role = 'alert';
+        latestCheck.innerHTML = '<button id="latest-dismiss" type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+            '<span aria-hidden="true">&times;</span></button>' +
+            'This documentation is for OpenLayers v<span id="package-version">' + currentVersion + '</span>. ' +
+            'The <a id="latest-link" href="#" class="alert-link">latest</a> is v<span id="latest-version"></span>.';
+        var alertContainer = document.getElementById('wrap').lastElementChild;
+        alertContainer.insertBefore(latestCheck, alertContainer.childNodes[0]);
+        document.getElementById('latest-version').innerHTML = latestVersion;
+        document.getElementById('latest-dismiss').onclick = function() {
+          latestCheck.style.display = 'none';
+          document.cookie = cookieText;
+        }
+      }
+
+      // create source code links to github
+      var srcLinks = $('div.tag-source');
+      var masterSearch = window.location.href.match(/\/([^\/]*\/)apidoc\//);
+      if (masterSearch && masterSearch.length) {
+        var branch = masterSearch[1];
+        if (branch == 'latest/') {
+          branch = 'v' + currentVersion + '/';
+        }
+        srcLinks.each(function(i, el) {
+          var textParts = el.innerHTML.trim().split(', ');
+          var link = 'https://github.com/openlayers/ol3/blob/' + branch +
+              textParts[0];
+          el.innerHTML = '<a href="' + link + '">' + textParts[0] + '</a>, ' +
+              '<a href="' + link + textParts[1].replace('line ', '#l') + '">' +
+              textParts[1] + '</a>';
+        });
+      }
+    };
+    xhr.send();
 
     // show/hide unstable items
     var links = $('a[href^="ol."]');
--- en/v3.15.1/apidoc/scripts/main.js
+++ en/v3.15.1/apidoc/scripts/main.js
@@ -53,11 +53,56 @@ $(function () {
     $(window).on('resize', _onResize);
     _onResize();
 
+    // warn about outdated version
+    var packageUrl = 'https://raw.githubusercontent.com/openlayers/openlayers.github.io/build/package.json';
+    var xhr = new XMLHttpRequest();
+    var currentVersion;
+    xhr.open('GET', packageUrl);
+    xhr.onload = function(response) {
+      var json = JSON.parse(xhr.responseText);
+      var latestVersion = json.version;
+      var url = window.location.href;
+      var branchSearch = url.match(/\/v([^\/]*)\/apidoc\//);
+      currentVersion = branchSearch[1];
+      var cookieText = 'dismissed=-' + latestVersion + '-';
+      var dismissed = document.cookie.indexOf(cookieText) != -1;
+      if (!dismissed && /^[0-9\.]*$/.test(currentVersion) && currentVersion != latestVersion) {
+        var link = url.replace(branchSearch[0], '/latest/apidoc/');
+        var linkXhr = new XMLHttpRequest();
+        linkXhr.open('HEAD', link);
+        linkXhr.onload = function(response) {
+          var a = document.getElementById('latest-link');
+          a.href = linkXhr.status == 200 ? link : '../../latest/apidoc/';
+        };
+        linkXhr.send();
+        var latestCheck = document.createElement('div');
+        latestCheck.style.marginTop = '-10px';
+        latestCheck.style.marginBottom = '10px';
+        latestCheck.className = 'alert alert-warning alert-dismissable';
+        latestCheck.role = 'alert';
+        latestCheck.innerHTML = '<button id="latest-dismiss" type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+            '<span aria-hidden="true">&times;</span></button>' +
+            'This documentation is for OpenLayers v<span id="package-version">' + currentVersion + '</span>. ' +
+            'The <a id="latest-link" href="#" class="alert-link">latest</a> is v<span id="latest-version"></span>.';
+        var alertContainer = document.getElementById('wrap').lastElementChild;
+        alertContainer.insertBefore(latestCheck, alertContainer.childNodes[0]);
+        document.getElementById('latest-version').innerHTML = latestVersion;
+        document.getElementById('latest-dismiss').onclick = function() {
+          latestCheck.style.display = 'none';
+          document.cookie = cookieText;
+        }
+      }
+    };
+    xhr.send();
+
     // create source code links to github
     var srcLinks = $('div.tag-source');
     var masterSearch = window.location.href.match(/\/([^\/]*\/)apidoc\//);
     if (masterSearch && masterSearch.length) {
       var branch = masterSearch[1];
+      if (branch == 'latest') {
+        branch = currentVersion;
+      }
       srcLinks.each(function(i, el) {
         var textParts = el.innerHTML.trim().split(', ');
         var link = 'https://github.com/openlayers/ol3/blob/' + branch +
