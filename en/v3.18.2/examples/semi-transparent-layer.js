3beb23c3483669275ee9b261898e2caedd4ab5c0
Andreas Hocevar <andreas.hocevar@gmail.com>
2016-09-01 17:02:14 +0200

    Updates

---
+++ en/v3.18.2/examples/semi-transparent-layer.js
@@ -0,0 +1,19 @@
+var map = new ol.Map({
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    }),
+    new ol.layer.Tile({
+      source: new ol.source.TileJSON({
+        url: 'https://api.tiles.mapbox.com/v3/mapbox.va-quake-aug.json?secure',
+        crossOrigin: 'anonymous'
+      })
+    })
+  ],
+  renderer: common.getRendererFromQueryString(),
+  target: 'map',
+  view: new ol.View({
+    center: ol.proj.fromLonLat([-77.93255, 37.9555]),
+    zoom: 7
+  })
+});
---
+++ en/v3.18.2/examples/side-by-side.js
@@ -0,0 +1,34 @@
+var map1 = new ol.Map({
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  renderer: 'dom',
+  target: 'domMap',
+  view: new ol.View({
+    center: [0, 0],
+    zoom: 1
+  })
+});
+
+var map2 = new ol.Map({
+  target: 'canvasMap',
+  layers: map1.getLayers(),
+  view: map1.getView()
+});
+
+if (ol.has.WEBGL) {
+  var map3 = new ol.Map({
+    renderer: 'webgl',
+    target: 'webglMap',
+    layers: map1.getLayers(),
+    view: map1.getView()
+  });
+} else {
+  var info = document.getElementById('no-webgl');
+  /**
+   * display error message
+   */
+  info.style.display = '';
+}
