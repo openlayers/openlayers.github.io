7761f8b3f054de820cf5151c3cc8fb960413e279
Tim Schaub <tim.schaub@gmail.com>
2017-04-13 21:36:42 -0600

    Updates

---
+++ en/v4.1.0/examples/draw-freehand.js
@@ -0,0 +1,44 @@
+var raster = new ol.layer.Tile({
+  source: new ol.source.OSM()
+});
+
+var source = new ol.source.Vector({wrapX: false});
+
+var vector = new ol.layer.Vector({
+  source: source
+});
+
+var map = new ol.Map({
+  layers: [raster, vector],
+  target: 'map',
+  view: new ol.View({
+    center: [-11000000, 4600000],
+    zoom: 4
+  })
+});
+
+var typeSelect = document.getElementById('type');
+
+var draw; // global so we can remove it later
+function addInteraction() {
+  var value = typeSelect.value;
+  if (value !== 'None') {
+    draw = new ol.interaction.Draw({
+      source: source,
+      type: /** @type {ol.geom.GeometryType} */ (typeSelect.value),
+      freehand: true
+    });
+    map.addInteraction(draw);
+  }
+}
+
+
+/**
+ * Handle change event.
+ */
+typeSelect.onchange = function() {
+  map.removeInteraction(draw);
+  addInteraction();
+};
+
+addInteraction();
---
+++ en/v4.1.0/examples/modify-features.js
@@ -0,0 +1,29 @@
+var raster = new ol.layer.Tile({
+  source: new ol.source.OSM()
+});
+
+var vector = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    url: 'data/geojson/countries.geojson',
+    format: new ol.format.GeoJSON(),
+    wrapX: false
+  })
+});
+
+var select = new ol.interaction.Select({
+  wrapX: false
+});
+
+var modify = new ol.interaction.Modify({
+  features: select.getFeatures()
+});
+
+var map = new ol.Map({
+  interactions: ol.interaction.defaults().extend([select, modify]),
+  layers: [raster, vector],
+  target: 'map',
+  view: new ol.View({
+    center: [0, 0],
+    zoom: 2
+  })
+});
