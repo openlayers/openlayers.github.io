4e4ecce1be5aaf277e7771bf1bba48cfeb5f2be9
Tim Schaub <tim@planet.com>
2017-05-03 09:36:10 -0600

    Updates

---
+++ en/v4.1.1/examples/drag-rotate-and-zoom.js
@@ -0,0 +1,15 @@
+var map = new ol.Map({
+  interactions: ol.interaction.defaults().extend([
+    new ol.interaction.DragRotateAndZoom()
+  ]),
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  target: 'map',
+  view: new ol.View({
+    center: [0, 0],
+    zoom: 2
+  })
+});
