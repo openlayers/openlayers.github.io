8d180635223a583921f7710d399b274cdd64c1b7
Tim Schaub <tim.schaub@gmail.com>
2017-08-31 16:34:40 -0600

    Updates

---
+++ en/v4.3.2/examples/canvas-gradient-pattern-require.js
@@ -0,0 +1,11 @@
+goog.require('ol.Map');
+goog.require('ol.View');
+goog.require('ol.extent');
+goog.require('ol.format.GeoJSON');
+goog.require('ol.has');
+goog.require('ol.layer.Vector');
+goog.require('ol.proj');
+goog.require('ol.source.Vector');
+goog.require('ol.style.Fill');
+goog.require('ol.style.Stroke');
+goog.require('ol.style.Style');
---
+++ en/v4.3.2/examples/static-image-require.js
@@ -0,0 +1,6 @@
+goog.require('ol.Map');
+goog.require('ol.View');
+goog.require('ol.extent');
+goog.require('ol.layer.Image');
+goog.require('ol.proj.Projection');
+goog.require('ol.source.ImageStatic');
