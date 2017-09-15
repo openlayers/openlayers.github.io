45f42af406aca6b598a821f9034203a624829250
Tim Schaub <tim.schaub@gmail.com>
2017-09-15 12:44:55 -0600

    Updates

---
+++ en/v4.3.3/examples/flight-animation-require.js
@@ -0,0 +1,11 @@
+goog.require('ol.Feature');
+goog.require('ol.Map');
+goog.require('ol.View');
+goog.require('ol.geom.LineString');
+goog.require('ol.layer.Tile');
+goog.require('ol.layer.Vector');
+goog.require('ol.proj');
+goog.require('ol.source.Stamen');
+goog.require('ol.source.Vector');
+goog.require('ol.style.Stroke');
+goog.require('ol.style.Style');
---
+++ en/v4.3.3/examples/geojson-vt-require.js
@@ -0,0 +1,8 @@
+goog.require('ol.Map');
+goog.require('ol.View');
+goog.require('ol.format.GeoJSON');
+goog.require('ol.source.OSM');
+goog.require('ol.source.VectorTile');
+goog.require('ol.layer.Tile');
+goog.require('ol.layer.VectorTile');
+goog.require('ol.proj.Projection');
