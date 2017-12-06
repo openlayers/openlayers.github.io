db5db1767d7436284a6f9163054bcd4be8b76368
Andreas Hocevar <andreas.hocevar@gmail.com>
2017-12-06 16:22:31 +0100

    Updates

---
+++ en/v4.6.0/examples/mouse-position-require.js
@@ -0,0 +1,7 @@
+goog.require('ol.Map');
+goog.require('ol.View');
+goog.require('ol.control');
+goog.require('ol.control.MousePosition');
+goog.require('ol.coordinate');
+goog.require('ol.layer.Tile');
+goog.require('ol.source.OSM');
