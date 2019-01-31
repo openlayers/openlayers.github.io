cdb833ae8dcc9c8bfe0cae96ffaf65e5c2993b18
ahocevar <andreas.hocevar@gmail.com>
2019-01-31 15:57:16 +0100

    Updates

--- en/master/examples/interaction-options.js
+++ en/master/examples/interaction-options.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[61],{290:function(n,e,o){"use strict";o.r(e);var a=o(2),s=o(3),t=o(52),w=o(6),c=o(11);new a.a({interactions:Object(t.a)({constrainResolution:!0,onFocusOnly:!0}),layers:[new w.a({source:new c.b})],target:"map",view:new s.a({center:[0,0],zoom:2})})}},[[290,0]]]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[61],{290:function(n,e,o){"use strict";o.r(e);var a=o(2),s=o(3),t=o(51),w=o(6),c=o(11);new a.a({interactions:Object(t.a)({constrainResolution:!0,onFocusOnly:!0}),layers:[new w.a({source:new c.b})],target:"map",view:new s.a({center:[0,0],zoom:2})})}},[[290,0]]]);
 //# sourceMappingURL=interaction-options.js.map
