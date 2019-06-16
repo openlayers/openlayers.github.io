163773eec8e0517b2195540a727b2f0f39800180
ahocevar <andreas.hocevar@gmail.com>
2019-06-16 09:21:14 +0200

    Updates

--- en/master/examples/tilejson.js
+++ en/master/examples/tilejson.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[129],{371:function(e,a,o){"use strict";o.r(a);var s=o(3),n=o(2),r=o(6),p=o(54);new s.a({layers:[new r.a({source:new p.a({url:"https://api.tiles.mapbox.com/v3/mapbox.geography-class.json?secure",crossOrigin:"anonymous"})})],target:"map",view:new n.a({center:[0,0],zoom:2})})}},[[371,0]]]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[129],{371:function(e,n,a){"use strict";a.r(n);var o=a(3),s=a(2),r=a(6),w=a(54);new o.a({layers:[new r.a({source:new w.a({url:"https://a.tiles.mapbox.com/v3/aj.1x1-degrees.json",crossOrigin:"anonymous"})})],target:"map",view:new s.a({center:[0,0],zoom:2})})}},[[371,0]]]);
 //# sourceMappingURL=tilejson.js.map
