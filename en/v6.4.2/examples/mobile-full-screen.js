1d62e48c84817dde227c858393e410ff74f1c494
Andreas Hocevar <andreas.hocevar@gmail.com>
2020-07-30 23:19:49 +0200

    Updates

---
+++ en/v6.4.2/examples/mobile-full-screen.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[96],{327:function(e,n,o){"use strict";o.r(n);var t=o(154),a=o(167),i=o(3),s=o(5),c=new(o(2).a)({center:[0,0],zoom:2}),r=(new i.a({layers:[new s.a({source:new t.a({key:"ApTJzdkyN1DdFKkRAE6QIDtzihNaf6IWJsT-nQ_2eMoO4PN__0Tzhl2-WgJtXFSp ",imagerySet:"RoadOnDemand"})})],target:"map",view:c}),new a.a({projection:c.getProjection(),tracking:!0}));r.once("change:position",(function(){c.setCenter(r.getPosition()),c.setResolution(2.388657133911758)}))}},[[327,0]]]);
+//# sourceMappingURL=mobile-full-screen.js.map
