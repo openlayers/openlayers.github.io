39d2aaf7e2ab8d81757f20c4d527567ba4c8e551
Andreas Hocevar <andreas.hocevar@gmail.com>
2020-12-27 20:18:12 +0100

    Updates

---
+++ en/v6.5.0/examples/accessible.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{237:function(e,o,n){"use strict";n.r(o);var t=n(3),c=n(9),w=n(5),i=n(2),m=new t.a({layers:[new w.a({source:new c.b})],target:"map",view:new i.a({center:[0,0],zoom:2})});document.getElementById("zoom-out").onclick=function(){var e=m.getView(),o=e.getZoom();e.setZoom(o-1)},document.getElementById("zoom-in").onclick=function(){var e=m.getView(),o=e.getZoom();e.setZoom(o+1)}}},[[237,0]]]);
+//# sourceMappingURL=accessible.js.map
