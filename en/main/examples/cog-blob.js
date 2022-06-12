508099467a9b9d75ff3bcd006a64e8b40fea5cff
Andreas Hocevar <andreas.hocevar@gmail.com>
2022-06-12 13:48:16 +0200

    Updates

---
+++ en/main/examples/cog-blob.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[6997],{159:function(e,n,t){var u=t(5877),r=t(1376),c=t(2139);fetch("data/example.tif").then((function(e){return e.blob()})).then((function(e){var n=new u.Z({sources:[{blob:e}]});new r.Z({target:"map",layers:[new c.Z({source:n})],view:n.getView().then((function(e){return e.showFullExtent=!0,e}))})}))}},function(e){var n=function(n){return e(e.s=n)};n(9877),n(159)}]);
+//# sourceMappingURL=cog-blob.js.map
