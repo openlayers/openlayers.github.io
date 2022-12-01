e9d03536bc700598084b8d06c0f5d3c246f497fb
Andreas Hocevar <andreas.hocevar@gmail.com>
2022-12-01 15:10:33 +0000

    Website updates

--- dist/en/main/ol/source/OGCMapTile.d.ts
+++ dist/en/main/ol/source/OGCMapTile.d.ts
@@ -97,6 +97,7 @@ export type Options = {
  * Layer source for map tiles from an [OGC API - Tiles](https://ogcapi.ogc.org/tiles/) service that provides "map" type tiles.
  * The service must conform to at least the core (http://www.opengis.net/spec/ogcapi-tiles-1/1.0/conf/core)
  * and tileset (http://www.opengis.net/spec/ogcapi-tiles-1/1.0/conf/tileset) conformance classes.
+ * @api
  */
 declare class OGCMapTile extends TileImage {
     /**
--- dist/en/main/ol/source/OGCVectorTile.d.ts
+++ dist/en/main/ol/source/OGCVectorTile.d.ts
@@ -101,6 +101,7 @@ export type Options = {
  * Vector tile sets may come in a variety of formats (e.g. GeoJSON, MVT).  The `format` option is used to determine
  * which of the advertised media types is used.  If you need to force the use of a particular media type, you can
  * provide the `mediaType` option.
+ * @api
  */
 declare class OGCVectorTile extends VectorTile {
     /**
