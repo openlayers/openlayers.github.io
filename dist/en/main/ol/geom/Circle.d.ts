3ba82523a74d9257748ce249983f1d037b6a0c8d
Andreas Hocevar <andreas.hocevar@gmail.com>
2023-07-28 09:03:07 +0000

    Website updates

--- dist/en/main/ol/geom/Circle.d.ts
+++ dist/en/main/ol/geom/Circle.d.ts
@@ -10,7 +10,7 @@ declare class Circle extends SimpleGeometry {
      * @param {!import("../coordinate.js").Coordinate} center Center.
      *     For internal use, flat coordinates in combination with `layout` and no
      *     `radius` are also accepted.
-     * @param {number} [radius] Radius.
+     * @param {number} [radius] Radius in units of the projection.
      * @param {import("./Geometry.js").GeometryLayout} [layout] Layout.
      */
     constructor(center: import("../coordinate.js").Coordinate, radius?: number | undefined, layout?: import("./Geometry.js").GeometryLayout | undefined);
