db3221734164ce1cfff763e322cb5142eb8b713c
Andreas Hocevar <andreas.hocevar@gmail.com>
2023-09-12 10:13:59 +0000

    Website updates

--- dist/en/main/ol/interaction/Link.d.ts
+++ dist/en/main/ol/interaction/Link.d.ts
@@ -1,5 +1,6 @@
 export default Link;
 export type Params = 'x' | 'y' | 'z' | 'r' | 'l';
+export type Callback = (arg0: string) => void;
 export type Options = {
     /**
      * Animate view transitions.
@@ -23,6 +24,9 @@ export type Options = {
     prefix?: string | undefined;
 };
 /** @typedef {'x'|'y'|'z'|'r'|'l'} Params */
+/**
+ * @typedef {function(string):void} Callback
+ */
 /**
  * @typedef {Object} Options
  * @property {boolean|import('../View.js').AnimationOptions} [animate=true] Animate view transitions.
@@ -80,6 +84,18 @@ declare class Link extends Interaction {
      */
     private updateState_;
     /**
+     * The tracked parameter callbacks.
+     * @private
+     * @type {Object<string, Callback>}
+     */
+    private trackedCallbacks_;
+    /**
+     * The tracked parameter values.
+     * @private
+     * @type {Object<string, string|null>}
+     */
+    private trackedValues_;
+    /**
      * @private
      * @param {string} name A parameter name.
      * @return {string} A name with the prefix applied.
@@ -120,9 +136,33 @@ declare class Link extends Interaction {
      */
     private handleChangeLayerGroup_;
     /**
+     * Register a listener for a URL search parameter.  The callback will be called with a new value
+     * when the corresponding search parameter changes due to history events (e.g. browser navigation).
+     *
+     * @param {string} key The URL search parameter.
+     * @param {Callback} callback The function to call when the search parameter changes.
+     * @return {string|null} The initial value of the search parameter (or null if absent from the URL).
+     * @api
+     */
+    track(key: string, callback: Callback): string | null;
+    /**
+     * Update the URL with a new search parameter value.  If the value is null, it will be
+     * deleted from the search parameters.
+     *
+     * @param {string} key The URL search parameter.
+     * @param {string|null} value The updated value (or null to remove it from the URL).
+     * @api
+     */
+    update(key: string, value: string | null): void;
+    /**
      * @private
      */
     private updateUrl_;
+    /**
+     * @private
+     * @param {URL} url The URL.
+     */
+    private updateHistory_;
 }
 import Interaction from './Interaction.js';
 //# sourceMappingURL=Link.d.ts.map
