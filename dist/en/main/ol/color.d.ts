c6a14c8a0ab682af5754665cc2e3c25254a916f0
Andreas Hocevar <andreas.hocevar@gmail.com>
2023-09-15 19:08:22 +0000

    Website updates

--- dist/en/main/ol/color.d.ts
+++ dist/en/main/ol/color.d.ts
@@ -1,10 +1,23 @@
 /**
+ * A color represented as a short array [red, green, blue, alpha].
+ * red, green, and blue should be integers in the range 0..255 inclusive.
+ * alpha should be a float in the range 0..1 inclusive. If no alpha value is
+ * given then `1` will be used.
+ * @typedef {Array<number>} Color
+ * @api
+ */
+/**
  * Return the color as an rgba string.
  * @param {Color|string} color Color.
  * @return {string} Rgba string.
  * @api
  */
 export function asString(color: Color | string): string;
+/**
+ * @param {string} s String.
+ * @return {Color} Color.
+ */
+export function fromString(s: string): Color;
 /**
  * Return the color as an array. This function maintains a cache of calculated
  * arrays which means the result should not be modified.
@@ -29,11 +42,6 @@ export function toString(color: Color): string;
  * @return {boolean} Whether the string is actually a valid color
  */
 export function isStringColor(s: string): boolean;
-/**
- * @param {string} s String.
- * @return {Color} Color.
- */
-export function fromString(s: string): Color;
 /**
  * A color represented as a short array [red, green, blue, alpha].
  * red, green, and blue should be integers in the range 0..255 inclusive.
