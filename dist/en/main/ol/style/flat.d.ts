6b2b9fb472fb89e20db7fb706e291cd9bd68bc21
Andreas Hocevar <andreas.hocevar@gmail.com>
2023-09-25 15:42:39 +0000

    Website updates

--- dist/en/main/ol/style/flat.d.ts
+++ dist/en/main/ol/style/flat.d.ts
@@ -27,19 +27,20 @@
  *
  *     const rules = [
  *       {
- *         filter: ['>', 'population', 1_000_000],
+ *         filter: ['>', ['get', 'population'], 1_000_000],
  *         style: {
  *           'circle-radius': 10,
- *           'circle-color': 'red',
+ *           'circle-fill-color': 'red',
  *         }
  *       },
  *       {
  *         else: true,
  *         style: {
  *           'circle-radius': 5,
- *           'circle-color': 'blue',
- *        },
- *      ];
+ *           'circle-fill-color': 'blue',
+ *         },
+ *       },
+ *     ];
  */
 /**
  * A literal boolean (e.g. `true`) or an expression that evaluates to a boolean (e.g. `['>', ['get', 'population'], 1_000_000]`).
