5fd87bc3097e523be3fc4a100e65cad5b9a1b8cc
Andreas Hocevar <andreas.hocevar@gmail.com>
2023-11-09 09:32:57 +0000

    Website updates

--- dist/en/main/ol/tilegrid.d.ts
+++ dist/en/main/ol/tilegrid.d.ts
@@ -56,6 +56,8 @@ export function createForProjection(projection: import("./proj.js").ProjectionLi
  * @return {import("./extent.js").Extent} Extent.
  */
 export function extentFromProjection(projection: import("./proj.js").ProjectionLike): import("./extent.js").Extent;
+export { TileGrid };
+export { default as WMTS } from "./tilegrid/WMTS.js";
 export type XYZOptions = {
     /**
      * Extent for the tile grid. The origin for an XYZ tile grid is the
