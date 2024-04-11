5cae49e73c2370c29709727b6db917b7b1087f6f
Andreas Hocevar <andreas.hocevar@gmail.com>
2024-04-11 08:48:34 +0000

    Website updates

--- dist/en/main/examples/mapbox-style.js
+++ dist/en/main/examples/mapbox-style.js
@@ -1,2 +1,2 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[6190],{43577:function(n,t,e){var o=e(5596);(0,e(99872).Ay)("map","https://api.maptiler.com/maps/topo-v2/style.json?key=get_your_own_D6rA4zTHduk6KOKTXzGB").then((function(n){n.addControl(new o.A)}))}},function(n){var t;t=43577,n(n.s=t)}]);
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[6190],{43577:function(t,e,n){var s=n(5596);(0,n(99872).Ay)("map","https://api.maptiler.com/maps/streets-v2/style.json?key=get_your_own_D6rA4zTHduk6KOKTXzGB").then((function(t){t.addControl(new s.A)}))}},function(t){var e;e=43577,t(t.s=e)}]);
 //# sourceMappingURL=mapbox-style.js.map
