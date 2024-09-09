51da9383ae3bf7a780d617bf2551ee92c49537c7
Andreas Hocevar <andreas.hocevar@gmail.com>
2024-09-09 17:07:00 +0000

    Website updates

--- dist/en/main/ol/format/WMSCapabilities.d.ts
+++ dist/en/main/ol/format/WMSCapabilities.d.ts
@@ -1,4 +1,19 @@
 export default WMSCapabilities;
+export type RootObject = {
+    /**
+     * Version
+     */
+    version: string;
+    /**
+     * Whether version is 1.3 or higher
+     */
+    v13: boolean;
+};
+/**
+ * @typedef {Object} RootObject
+ * @property {string} version Version
+ * @property {boolean} v13 Whether version is 1.3 or higher
+ */
 /**
  * @classdesc
  * Format for reading WMS capabilities data
