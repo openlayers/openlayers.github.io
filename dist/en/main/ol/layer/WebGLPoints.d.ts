8ab6c3bf68ace28601021ef1df12567d46a4e3a8
Tim Schaub <tschaub@users.noreply.github.com>
2025-01-07 21:58:32 +0000

    Website updates

--- dist/en/main/ol/layer/WebGLPoints.d.ts
+++ dist/en/main/ol/layer/WebGLPoints.d.ts
@@ -144,6 +144,7 @@ export type Options<VectorSourceType extends import("../source/Vector.js").defau
  * @extends {Layer<VectorSourceType, WebGLPointsLayerRenderer>}
  * @fires import("../render/Event.js").RenderEvent#prerender
  * @fires import("../render/Event.js").RenderEvent#postrender
+ * @deprecated Use ol/layer/WebGLVector instead
  */
 declare class WebGLPointsLayer<VectorSourceType extends import("../source/Vector.js").default<import("../Feature").FeatureLike>> extends Layer<VectorSourceType, WebGLPointsLayerRenderer> {
     /**
