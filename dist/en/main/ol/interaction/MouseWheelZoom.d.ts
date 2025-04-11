640d00908a4ec60b368727eb7d0707571f7870bc
Tim Schaub <tschaub@users.noreply.github.com>
2025-04-11 08:51:07 +0000

    Website updates

--- dist/en/main/ol/interaction/MouseWheelZoom.d.ts
+++ dist/en/main/ol/interaction/MouseWheelZoom.d.ts
@@ -39,27 +39,6 @@ export type Options = {
     constrainResolution?: boolean | undefined;
 };
 /**
- * @typedef {'trackpad' | 'wheel'} Mode
- */
-/**
- * @typedef {Object} Options
- * @property {import("../events/condition.js").Condition} [condition] A function that
- * takes a {@link module:ol/MapBrowserEvent~MapBrowserEvent} and returns a
- * boolean to indicate whether that event should be handled. Default is
- * {@link module:ol/events/condition.always}.
- * @property {boolean} [onFocusOnly=false] When the map's target has a `tabindex` attribute set,
- * the interaction will only handle events when the map has the focus.
- * @property {number} [maxDelta=1] Maximum mouse wheel delta.
- * @property {number} [duration=250] Animation duration in milliseconds.
- * @property {number} [timeout=80] Mouse wheel timeout duration in milliseconds.
- * @property {boolean} [useAnchor=true] Enable zooming using the mouse's
- * location as the anchor. When set to `false`, zooming in and out will zoom to
- * the center of the screen instead of zooming on the mouse's location.
- * @property {boolean} [constrainResolution=false] If true, the mouse wheel zoom
- * event will always animate to the closest zoom level after an interaction;
- * false means intermediary zoom levels are allowed.
- */
-/**
  * @classdesc
  * Allows the user to zoom the map by scrolling the mouse wheel.
  * @api
