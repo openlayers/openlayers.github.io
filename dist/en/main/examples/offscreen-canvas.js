e4a8303cec87e066ed2ac2b81d476b4387ca5db6
Andreas Hocevar <andreas.hocevar@gmail.com>
2026-07-26 22:43:28 +0000

    Website updates

--- dist/en/main/examples/offscreen-canvas.js
+++ dist/en/main/examples/offscreen-canvas.js
@@ -2,7 +2,7 @@ import { Dn as createXYZ, En as Source, Hn as Layer, L as FullScreen, Mn as Map,
 //#region examples/offscreen-canvas.js
 var worker = new Worker(new URL(
 	/* @vite-ignore */
-	"/assets/offscreen-canvas.worker-CjIbew1N.js",
+	"/assets/offscreen-canvas.worker-W749btye.js",
 	"" + import.meta.url
 ), { type: "module" });
 var container;
